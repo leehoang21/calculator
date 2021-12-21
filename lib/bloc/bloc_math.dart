6b20a94646269bf84b6a7f68bd1970d75292f424
leehoang21 <88425870+leehoang21@users.noreply.github.com>
2021-12-21 15:14:34 +0700

    Add files via upload

--- lib/bloc/bloc_math.dart
+++ lib/bloc/bloc_math.dart
@@ -1,22 +1,24 @@
 import 'math.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class BlocMath extends Bloc<MathEvent, MathState> {
-  bool flagRemove = false;
+  String _result = '';
+
+  String get result => _result;
 
   BlocMath() : super(Clear()) {
     on<OrtherButtonsTapped>((event, emit) {
-      if (state.result == 'lỗi rồi') {
-        emit(Clear(userInput: state.userInput));
-      } else if (state.result != '') {
-        if (flagRemove) {
-          flagRemove = false;
-        } else if (!isOperator(event)) {
-          emit(AddInput(userInput: state.result));
+      if (_result == 'lỗi rồi') {
+        _result = '';
+        emit(Clear());
+      } else if (_result != '') {
+        if (isOperator(event)) {
+          emit(AddInput(userInput: _result));
         } else {
           emit(Clear());
         }
-        emit(Clear(userInput: state.userInput));
+        _result = '';
       }
       emit(
         AddInput(userInput: state.userInput + event.charUserInput),
@@ -25,37 +27,53 @@ class BlocMath extends Bloc<MathEvent, MathState> {
 
     on<ClearButtonTapped>(
       (event, emit) {
+        _result = '';
         emit(Clear());
       },
     );
 
-    on<DeleteButtonTapped>((event, emit) {
-      flagRemove = true;
-      emit(RemoveLast(userInput: state.userInput));
-    });
+    on<DeleteButtonTapped>(
+      (event, emit) => emit(
+        RemoveLast(
+          userInput: state.userInput.substring(
+            0,
+            state.userInput.length - 1,
+          ),
+        ),
+      ),
+    );
 
     on<EqualButtonTapped>((event, emit) {
-      if (state.userInput[0] == '+' && isNumber1()) {
-        emit(
-          AddInput(
-            userInput: state.userInput.substring(
-              1,
-              state.userInput.length,
+      try {
+        if (state.userInput[0] == '+' && isNumber()) {
+          emit(
+            AddInput(
+              userInput: state.userInput.substring(
+                1,
+                state.userInput.length,
+              ),
             ),
-          ),
-        );
+          );
+        }
+        double eval = Parser().parse(state.userInput).evaluate(
+              EvaluationType.REAL,
+              ContextModel(),
+            );
+        _result = eval.toString();
+      } catch (_) {
+        _result = 'lỗi rồi';
       }
       emit(Result(userInput: state.userInput));
     });
   }
 
-  bool isNumber1() {
+  bool isNumber() {
     return state.userInput[1].compareTo('9') <= 0 &&
         state.userInput[1].compareTo('0') >= 0;
   }
 
   bool isOperator(OrtherButtonsTapped event) {
-    return (event.charUserInput.compareTo('9') <= 0 &&
+    return !(event.charUserInput.compareTo('9') <= 0 &&
             event.charUserInput.compareTo('0') >= 0) ||
         event.charUserInput.compareTo('.') == 0;
   }
